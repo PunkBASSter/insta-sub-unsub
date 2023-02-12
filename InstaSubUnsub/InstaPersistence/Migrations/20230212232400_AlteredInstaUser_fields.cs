0528c3211ce86790dad35c57c1d4d5c0fd785196
Andrei <stdrew90@gmail.com>
2023-02-13 00:24:50 +0100

    Added InstaUsers model update

---
+++ InstaSubUnsub/InstaPersistence/Migrations/20230212232400_AlteredInstaUser_fields.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstaPersistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlteredInstaUserfields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FollowingDate",
+                table: "InstaUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UnfollowingDate",
+                table: "InstaUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FollowingDate",
+                table: "InstaUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UnfollowingDate",
+                table: "InstaUsers");
+        }
+    }
+}
