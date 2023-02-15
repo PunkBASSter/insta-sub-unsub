7119246c3663a58c71fb0baa37ddd23407c6a8b7
Andrei <stdrew90@gmail.com>
2023-02-15 06:58:15 +0100

    Added reading string constants from config

--- InstaSubUnsub/InstaCrawlerApp/UserCrawler.cs
+++ InstaSubUnsub/InstaCrawlerApp/UserCrawler.cs
@@ -1,5 +1,6 @@
 ﻿using InstaCrawlerApp.PersistenceInterfaces;
 using InstaDomain;
+using Microsoft.Extensions.Configuration;
 using SeleniumUtils.PageObjects;
 
 namespace InstaCrawlerApp
@@ -8,16 +9,20 @@ namespace InstaCrawlerApp
     {
         private readonly LoginPage _loginPage;
         private readonly FollowingPage _followingPage;
-        private readonly string _serviceUsername = "hidethetrack123";
-        private readonly string _servicePassword = "Imnot4insta!1";
+        private readonly Lazy<string> _serviceUsername; //= "hidethetrack123";
+        private readonly Lazy<string> _servicePassword; // = "Imnot4insta!1";
         private bool _isInitialized = false;
         private readonly IRepository<InstaUser> _instaUsersRepo;
+        private readonly IConfiguration _configuration;
 
-        public UserCrawler(LoginPage loginPage, FollowingPage followingPage, IRepository<InstaUser> repo)
+        public UserCrawler(LoginPage loginPage, FollowingPage followingPage, IRepository<InstaUser> repo, IConfiguration configuration)
         {
             _loginPage = loginPage;
             _followingPage = followingPage;
             _instaUsersRepo = repo;
+            _configuration = configuration;
+            _serviceUsername = new Lazy<string>(() => _configuration.GetSection("ServiceUser:Username").Value ?? throw new ArgumentException("ServiceUsername is not provided"));
+            _servicePassword = new Lazy<string>(() => _configuration.GetSection("ServiceUser:Password").Value ?? throw new ArgumentException("ServiceUsername is not provided"));
         }
 
         /// <summary>
@@ -29,7 +34,7 @@ namespace InstaCrawlerApp
             if (_isInitialized) return;
 
             _loginPage.Load();
-            _loginPage.Login(_serviceUsername, _servicePassword);
+            _loginPage.Login(_serviceUsername.Value, _servicePassword.Value);
             _loginPage.HandleAfrerLoginQuestions();
 
             //todo save cookies
@@ -43,7 +48,7 @@ namespace InstaCrawlerApp
         public void Crawl()
         {
             //test DB
-            var testUsr = new InstaUser { Name = _serviceUsername, Status = InstaDomain.Enums.UserStatus.Protected };
+            var testUsr = new InstaUser { Name = _serviceUsername.Value, Status = InstaDomain.Enums.UserStatus.Protected };
             //todo normal async
             var id = _instaUsersRepo.CreateAsync(testUsr, CancellationToken.None).Result;
             var readUser = _instaUsersRepo.Query.Take(10).ToList();
