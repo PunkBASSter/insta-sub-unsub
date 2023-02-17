77cffa56d7becbb7aea4862d5ddea8ba72291590
Andrei <stdrew90@gmail.com>
2023-02-17 21:55:27 +0100

    Updated protected users seed

--- InstaSubUnsub/InstaPersistence/DataSeed/ProtectedInstaUsers.cs
+++ InstaSubUnsub/InstaPersistence/DataSeed/ProtectedInstaUsers.cs
@@ -12,6 +12,7 @@ namespace InstaPersistence.DataSeed
                 new InstaUser { Id = 1, Name = "hidethetrack123", IsFollower = true, Status = UserStatus.Protected },
                 new InstaUser { Id = 2, Name = "doctor_lilith", IsFollower = true, Status = UserStatus.Protected },
                 new InstaUser { Id = 3, Name = "dr.imiller", IsFollower = true, Status = UserStatus.Protected },
+
                 new InstaUser { Id = 4, Name = "mynameiswhm", IsFollower = true, Status = UserStatus.Protected },
                 new InstaUser { Id = 5, Name = "err_yep", IsFollower = true, Status = UserStatus.Protected },
                 new InstaUser { Id = 6, Name = "temapunk", IsFollower = true, Status = UserStatus.Protected },
@@ -26,7 +27,14 @@ namespace InstaPersistence.DataSeed
                 new InstaUser { Id = 15, Name = "lodkaissad", IsFollower = true, Status = UserStatus.Protected },
                 new InstaUser { Id = 16, Name = "anastasiya_kun", IsFollower = true, Status = UserStatus.Protected },
                 new InstaUser { Id = 17, Name = "anna.saulenko", IsFollower = true, Status = UserStatus.Protected },
-                new InstaUser { Id = 18, Name = "prikhodko5139", IsFollower = true, Status = UserStatus.Protected }
+                new InstaUser { Id = 18, Name = "prikhodko5139", IsFollower = true, Status = UserStatus.Protected },
+                new InstaUser { Id = 19, Name = "saulenkosvetlana", IsFollower = true, Status = UserStatus.Protected },
+                new InstaUser { Id = 20, Name = "ira_knows_best", IsFollower = true, Status = UserStatus.Protected },
+
+                new InstaUser { Id = 21, Name = "meltali_handmade", IsFollower = true, Status = UserStatus.Protected },
+                new InstaUser { Id = 22, Name = "dr.ksusha_pro_edu", IsFollower = true, Status = UserStatus.Protected },
+                new InstaUser { Id = 23, Name = "lydok87", IsFollower = true, Status = UserStatus.Protected },
+
 
                 //TODO Add the rest of subscriptions for testing purposes for my account,
                 //Get protected subs for Ira's account
---
+++ InstaSubUnsub/InstaPersistence/Migrations/20230217205231_ProtectedInstaUsersUpdated2.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace InstaPersistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProtectedInstaUsersUpdated2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "InstaUsers",
+                columns: new[] { "Id", "Followers", "Following", "FollowingDate", "IsFollower", "Name", "Rank", "Status", "UnfollowingDate" },
+                values: new object[,]
+                {
+                    { 19L, 0, 0, null, true, "saulenkosvetlana", 0, 3, null },
+                    { 20L, 0, 0, null, true, "ira_knows_best", 0, 3, null },
+                    { 21L, 0, 0, null, true, "meltali_handmade", 0, 3, null },
+                    { 22L, 0, 0, null, true, "dr.ksusha_pro_edu", 0, 3, null },
+                    { 23L, 0, 0, null, true, "lydok87", 0, 3, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "InstaUsers",
+                keyColumn: "Id",
+                keyValue: 19L);
+
+            migrationBuilder.DeleteData(
+                table: "InstaUsers",
+                keyColumn: "Id",
+                keyValue: 20L);
+
+            migrationBuilder.DeleteData(
+                table: "InstaUsers",
+                keyColumn: "Id",
+                keyValue: 21L);
+
+            migrationBuilder.DeleteData(
+                table: "InstaUsers",
+                keyColumn: "Id",
+                keyValue: 22L);
+
+            migrationBuilder.DeleteData(
+                table: "InstaUsers",
+                keyColumn: "Id",
+                keyValue: 23L);
+        }
+    }
+}
