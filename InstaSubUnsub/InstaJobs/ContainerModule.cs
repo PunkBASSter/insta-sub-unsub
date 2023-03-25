868256514f8ddaa5261a0510017e727de57ad86c
Andrei <stdrew90@gmail.com>
2023-03-25 10:58:27 +0100

    Updated MaxConcurrency to 1 until AccountPool lock works properly.

--- InstaSubUnsub/InstaJobs/ContainerModule.cs
+++ InstaSubUnsub/InstaJobs/ContainerModule.cs
@@ -36,15 +36,16 @@ namespace InstaJobs
                 q.UseInMemoryStore();
                 q.UseDefaultThreadPool(tp =>
                 {
-                    tp.MaxConcurrency = 4; // 1 for each possible job
+                    //has to be one until AccountPool lock does not work correctly
+                    tp.MaxConcurrency = 1; // 1 for each possible job
                 });
 
                 // quickest way to create a job with single trigger is to use ScheduleJob
                 // (requires version 3.2)
 
-                q.ScheduleJob<QuartzJobWrapper<UserCrawler>>(trigger => trigger.WithIdentity(nameof(UserCrawler)).StartNow());
-                q.ScheduleJob<QuartzJobWrapper<UserFullDetailsProvider>>(trigger => trigger.WithIdentity(nameof(UserFullDetailsProvider)).StartNow());
-                //q.ScheduleJob<QuartzJobWrapper<Follower>>(trigger => trigger.WithIdentity(nameof(Follower)).StartNow());
+                //q.ScheduleJob<QuartzJobWrapper<UserCrawler>>(trigger => trigger.WithIdentity(nameof(UserCrawler)).StartNow());
+                //q.ScheduleJob<QuartzJobWrapper<UserFullDetailsProvider>>(trigger => trigger.WithIdentity(nameof(UserFullDetailsProvider)).StartNow());
+                q.ScheduleJob<QuartzJobWrapper<Follower>>(trigger => trigger.WithIdentity(nameof(Follower)).StartNow());
                 //q.ScheduleJob<QuartzJobWrapper<Unfollower>>(trigger => trigger.WithIdentity(nameof(Unfollower)).StartNow());
 
 
